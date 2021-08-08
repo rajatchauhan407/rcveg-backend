f431a406a4a37db54e55ba2521efc0a33c2c5e32
RAJAT CHAUHAN <rajatchauhan407@gmail.com>
2021-08-08 11:04:09 +0530

    first-commit

---
+++ routes/vegRoutes.js
@@ -0,0 +1,25 @@
+const express= require('express');
+const router = express.Router();
+const vegController= require('../controllers/vegPrices');
+const checkAuth = require('../middleware/auth-check');
+
+
+router.get("/check",vegController.check);
+router.post("/prices",vegController.prices);
+
+router.get("/prices",vegController.getPrices);
+
+router.post("/bucket",vegController.addVegBucket);
+
+router.get("/orders",vegController.getOrders);
+
+router.post("/orders/confirmOrder", vegController.confirmOrder);
+
+router.get("/orders/getInvoice", vegController.getInvoice);
+
+router.get("/bucket",vegController.getVegBucket);
+
+router.delete("/bucket/delete", vegController.bucketDelete);
+
+
+module.exports=router;
